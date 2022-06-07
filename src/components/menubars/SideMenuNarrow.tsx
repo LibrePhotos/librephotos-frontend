d5f581a7b666f9a4e36ae2cfea58590e643dae75
sickelap <sickelap@pm.me>
2022-06-07 23:57:10 +0100

    initial navigation rework (wip)

--- src/components/menubars/SideMenuNarrow.tsx
+++ src/components/menubars/SideMenuNarrow.tsx
@@ -1,133 +1,98 @@
-import { MediaQuery, Navbar } from "@mantine/core";
+import { createStyles, Text, Navbar } from "@mantine/core";
 import { useMediaQuery } from "@mantine/hooks";
-import React from "react";
+import { push } from "connected-react-router";
+import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
-import { Link } from "react-router-dom";
-import { Dropdown, Icon } from "semantic-ui-react";
-import { Album, ChartLine, FaceId, Photo, Trash, Users } from "tabler-icons-react";
+import { useLocation } from "react-router-dom";
+import { Album, Photo, Trash, Search, Share } from "tabler-icons-react";
 
-import { selectAuthAccess, selectIsAuthenticated } from "../../store/auth/authSelectors";
-import { useAppSelector } from "../../store/store";
-import { MainLink } from "./MenuLink";
+import { useAppDispatch } from "../../store/store";
+
+const useStyles = createStyles((theme, _params, getRef) => {
+  const icon = getRef('icon');
+  return {
+    link: {
+      ...theme.fn.focusStyles(),
+      display: 'flex',
+      alignItems: 'center',
+      textDecoration: 'none',
+      fontSize: theme.fontSizes.sm,
+      color: theme.colorScheme === 'dark' ? theme.colors.dark[1] : theme.colors.gray[7],
+      padding: `${theme.spacing.xs}px ${theme.spacing.sm}px`,
+      borderRadius: theme.radius.sm,
+      fontWeight: 500,
+
+      '&:hover': {
+        backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
+        color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+
+        [`& .${icon}`]: {
+          color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+        },
+      },
+    },
+
+    linkIcon: {
+      ref: icon,
+      color: theme.colorScheme === 'dark' ? theme.colors.dark[2] : theme.colors.gray[6],
+      marginRight: theme.spacing.sm,
+    },
+
+    linkActive: {
+      '&, &:hover': {
+        backgroundColor:
+          theme.colorScheme === 'dark'
+            ? theme.fn.rgba(theme.colors[theme.primaryColor][8], 0.25)
+            : theme.colors[theme.primaryColor][0],
+        color: theme.colorScheme === 'dark' ? theme.white : theme.colors[theme.primaryColor][7],
+        [`& .${icon}`]: {
+          color: theme.colors[theme.primaryColor][theme.colorScheme === 'dark' ? 5 : 7],
+        },
+      },
+    }
+  };
+});
 
 export function SideMenuNarrow(): JSX.Element {
-  const isAuth = useAppSelector(selectIsAuthenticated);
-  const access = useAppSelector(selectAuthAccess);
-
-  const matches = useMediaQuery("(min-width: 700px)");
+  const dispatch = useAppDispatch();
+  const isDesktop = useMediaQuery("(min-width: 700px)");
+  const location = useLocation();
   const { t } = useTranslation();
+  const { classes, cx } = useStyles();
+  const [ active, setActive ] = useState<string>(location.pathname);
 
-  if (!matches) {
+  const navigation = [
+    { link: '/', label: t('sidemenu.photos'), icon: Photo },
+    { link: '/useralbums', label: t('sidemenu.albums'), icon: Album },
+    { link: '/explore', label: t('sidemenu.explore'), icon: Search },
+    { link: '/shared/fromme/photos', label: t('sidemenu.sharing'), icon: Share },
+    { link: '/deleted', label: t('photos.deleted'), icon: Trash },
+  ];
+  const links = navigation.map(item => (
+    <Text<'a'>
+      className={cx(classes.link, { [classes.linkActive]: active === item.link })}
+      component="a"
+      href={item.link}
+      key={item.label}
+      onClick={(event) => {
+        event.preventDefault();
+        setActive(item.link);
+        dispatch(push(item.link));
+      }}
+    >
+      <item.icon className={classes.linkIcon} />
+      <span>{item.label}</span>
+    </Text>
+  ));
+
+  if (!isDesktop) {
     return <div></div>;
   }
 
   return (
-    <Navbar p="sm" hidden={false} width={{ base: 100 }}>
-      <Navbar.Section>
-        <MainLink icon={<Photo size={33}></Photo>} color="green" label={t("sidemenu.photos")} to="/" />
-      </Navbar.Section>
-      <Navbar.Section>
-        <Dropdown
-          pointing="left"
-          item
-          icon={<MainLink icon={<Album size={33}></Album>} color="blue" label={t("sidemenu.albums")} />}
-        >
-          <Dropdown.Menu>
-            <Dropdown.Header>{t("sidemenu.albums")}</Dropdown.Header>
-            <Dropdown.Item as={Link} to="/people">
-              <Icon name="users" />
-              {`  ${t("sidemenu.people")}`}
-            </Dropdown.Item>
-            <Dropdown.Item as={Link} to="/places">
-              <Icon name="map" />
-              {`  ${t("sidemenu.places")}`}
-            </Dropdown.Item>
-            <Dropdown.Item as={Link} to="/things">
-              <Icon name="tags" />
-              {`  ${t("sidemenu.things")}`}
-            </Dropdown.Item>
-            <Dropdown.Divider />
-            <Dropdown.Item as={Link} to="/useralbums">
-              <Icon name="bookmark" />
-              {`  ${t("sidemenu.myalbums")}`}
-            </Dropdown.Item>
-            <Dropdown.Item as={Link} to="/events">
-              <Icon name="wizard" />
-              {`  ${t("sidemenu.autoalbums")}`}
-            </Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
-      </Navbar.Section>
-      <Navbar.Section>
-        <Dropdown
-          pointing="left"
-          item
-          icon={<MainLink icon={<ChartLine size={33}></ChartLine>} color="yellow" label={t("sidemenu.datavizsmall")} />}
-        >
-          <Dropdown.Menu>
-            <Dropdown.Header>
-              <div style={{ overflow: "visible" }}>{t("sidemenu.dataviz")}</div>
-            </Dropdown.Header>
-            <Dropdown.Item as={Link} to="/placetree">
-              <Icon name="sitemap" />
-              {`  ${t("sidemenu.placetree")}`}
-            </Dropdown.Item>
-
-            <Dropdown.Item as={Link} to="/wordclouds">
-              <Icon name="cloud" />
-              {`  ${t("sidemenu.wordclouds")}`}
-            </Dropdown.Item>
-
-            <Dropdown.Item as={Link} to="/timeline">
-              <Icon name="chart bar" />
-              {`  ${t("sidemenu.timeline")}`}
-            </Dropdown.Item>
-
-            <Dropdown.Item as={Link} to="/socialgraph">
-              <Icon name="share alternate" />
-              {`  ${t("sidemenu.socialgraph")}`}
-            </Dropdown.Item>
-
-            <Dropdown.Item as={Link} to="/facescatter">
-              <Icon name="user circle" />
-              {`  ${t("sidemenu.facecluster")}`}
-            </Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
-      </Navbar.Section>
-      <Navbar.Section>
-        <MainLink icon={<FaceId size={33}></FaceId>} color="orange" label={t("sidemenu.facerecognition")} to="/faces" />
-      </Navbar.Section>
-      {isAuth && (
-        <Navbar.Section>
-          <Dropdown
-            pointing="left"
-            item
-            icon={<MainLink icon={<Users size={33}></Users>} color="red" label={t("sidemenu.sharing")} />}
-          >
-            <Dropdown.Menu>
-              <Dropdown.Header>{t("sidemenu.sharing")}</Dropdown.Header>
-
-              <Dropdown.Item disabled={!access} as={Link} to="/users/">
-                <Icon name="globe" />
-                {`  ${t("sidemenu.publicphotos")}`}
-              </Dropdown.Item>
-
-              <Dropdown.Item as={Link} to="/shared/fromme/photos/">
-                <Icon name="share" color="red" />
-                {`  ${t("sidemenu.youshared")}`}
-              </Dropdown.Item>
-
-              <Dropdown.Item as={Link} to="/shared/tome/photos/">
-                <Icon name="share" color="green" />
-                {`  ${t("sidemenu.sharedwithyou")}`}
-              </Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
-        </Navbar.Section>
-      )}
-      <Navbar.Section>
-        <MainLink icon={<Trash size={33}></Trash>} color="black" label={t("photos.deleted")} to="/deleted" />
+    <Navbar width={{ sm: 250 }} style={{ paddingTop: 20 }}>
+      <Navbar.Section grow>
+        {links}
       </Navbar.Section>
     </Navbar>
   );
