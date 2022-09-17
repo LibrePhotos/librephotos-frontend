c417f7746e8f55081e81fce4b0d7697085fc1e5f
sickelap <sickelap@pm.me>
2022-09-17 18:27:36 +0100

    fix custom search for mantine 5

--- src/components/CustomSearch.tsx
+++ src/components/CustomSearch.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Group, Loader, Menu, Popover, TextInput } from "@mantine/core";
+import { Avatar, Group, Loader, Popover, TextInput, createStyles } from "@mantine/core";
 import React, { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
@@ -39,19 +39,53 @@ function PersonAvatar({ person }: PersonAvatarProps) {
           to={`/person/${person.key}`}
           onMouseEnter={() => setOpened(true)}
           onMouseLeave={() => setOpened(false)}
-          key={`suggestion_person_${person.key}`}
           size={50}
           radius="xl"
           src={serverAddress + person.face_url}
         />
       </Popover.Target>
-      {person.text}
+      <Popover.Dropdown>{person.text}</Popover.Dropdown>
     </Popover>
   );
 }
 
+const useStyle = createStyles(theme => ({
+  item: {
+    ...theme.fn.focusStyles(),
+    display: "flex",
+    alignItems: "center",
+    textDecoration: "none",
+    fontSize: theme.fontSizes.sm,
+    color: theme.colorScheme === "dark" ? theme.colors.dark[1] : theme.colors.gray[7],
+    padding: `${theme.spacing.xs}px ${theme.spacing.sm}px`,
+    fontWeight: 500,
+    cursor: "pointer",
+
+    "&:hover": {
+      backgroundColor: theme.colorScheme === "dark" ? theme.colors.dark[4] : theme.colors.gray[1],
+      color: theme.colorScheme === "dark" ? theme.white : theme.black,
+      a: {
+        color: theme.colorScheme === "dark" ? theme.white : theme.black,
+      },
+    },
+
+    a: {
+      textDecoration: "none",
+      color: theme.colorScheme === "dark" ? theme.colors.dark[1] : theme.colors.gray[7],
+    },
+  },
+  dropdownWrapper: {
+    padding: 2,
+  },
+  faces: {
+    padding: "5px 2px",
+  },
+}));
+
 export function CustomSearch() {
   const { t } = useTranslation();
+  const { classes } = useStyle();
+  const [suggestionsOpen, setSuggestionsOpen] = useState(false);
   const [searchText, setSearchText] = useState("");
   const [exampleSearchTerm, setExampleSearchTerm] = useState("");
   const [filteredExampleSearchTerms, setFilteredExampleSearchTerms] = useState<any[]>([]);
@@ -75,9 +109,7 @@ export function CustomSearch() {
       const searchTerm = example ? `${t("search.search")} ${example}` : t("search.default");
       setExampleSearchTerm(searchTerm || "");
     }, 5000);
-    return () => {
-      clearInterval(interval);
-    };
+    return () => clearInterval(interval);
   }, [exampleSearchTerms, t]);
 
   useEffect(() => {
@@ -108,7 +140,7 @@ export function CustomSearch() {
       filterExamples = exampleSearchTerms.filter((el: string) =>
         fuzzyMatch(el.toLowerCase(), searchText.toLowerCase())
       );
-      filterPeople = filterPeople.filter((person: { text: string }) =>
+      filterPeople = people.filter((person: { text: string }) =>
         fuzzyMatch(person.text.toLowerCase(), searchText.toLowerCase())
       );
       filterPlaces = albumsPlaceList.filter((place: { title: string }) =>
@@ -135,89 +167,89 @@ export function CustomSearch() {
   };
 
   return (
-    <Menu>
-      <Menu.Target>
-        <TextInput
-          icon={<Search size={14} />}
-          onKeyPress={event => {
-            switch (event.key) {
-              case "Enter":
-                dispatch(searchPhotos(searchText));
-                dispatch(push("/search"));
-                break;
-              default:
-                break;
-            }
-          }}
-          onChange={handleChange}
-          placeholder={exampleSearchTerm}
-        />
-      </Menu.Target>
-      {filteredExampleSearchTerms.length > 0 &&
-        filteredExampleSearchTerms.slice(0, 2).map(el => (
-          <Menu.Item
-            component={Link}
-            to="/search"
+    <Popover width="target" withinPortal opened={suggestionsOpen} offset={2}>
+      <Popover.Target>
+        <div onFocusCapture={() => setSuggestionsOpen(true)} onBlurCapture={() => setSuggestionsOpen(false)}>
+          <TextInput
             icon={<Search size={14} />}
-            key={`suggestion_${el}`}
+            onKeyPress={event => {
+              switch (event.key) {
+                case "Enter":
+                  dispatch(searchPhotos(searchText));
+                  dispatch(push("/search"));
+                  break;
+                default:
+                  break;
+              }
+            }}
+            onChange={handleChange}
+            placeholder={exampleSearchTerm}
+          />
+        </div>
+      </Popover.Target>
+
+      <Popover.Dropdown className={classes.dropdownWrapper}>
+        {filteredExampleSearchTerms.slice(0, 2).map(example => (
+          <Group
+            className={classes.item}
+            key={`suggestion_${example}`}
             onClick={() => {
-              dispatch(searchPhotos(el));
-              dispatch(searchPeople(el));
-              dispatch(searchThingAlbums(el));
-              dispatch(searchPlaceAlbums(el));
+              dispatch(searchPhotos(example));
+              dispatch(searchPeople(example));
+              dispatch(searchThingAlbums(example));
+              dispatch(searchPlaceAlbums(example));
             }}
           >
-            {el}
-          </Menu.Item>
+            <Search size={14} />
+            {example}
+          </Group>
         ))}
-      {filteredSuggestedUserAlbums.length > 0 &&
-        filteredSuggestedUserAlbums.slice(0, 2).map(album => (
-          <Menu.Item
-            component={Link}
-            to={`/useralbum/${album.id}`}
-            icon={<Album size={14} />}
-            key={`suggestion_place_${album.title}`}
-          >
-            {album.title}
-          </Menu.Item>
-        ))}
-      {filteredSuggestedPlaces.length > 0 &&
-        filteredSuggestedPlaces.slice(0, 2).map(place => (
-          <Menu.Item
-            component={Link}
-            to={`/place/${place.id}`}
-            icon={<Map size={14} />}
-            key={`suggestion_place_${place.title}`}
-          >
-            {place.title}
-          </Menu.Item>
-        ))}
-      {filteredSuggestedThings.length > 0 &&
-        filteredSuggestedThings.slice(0, 2).map(thing => (
-          <Menu.Item
-            component={Link}
-            to={`/thing/${thing.id}`}
-            icon={<Tag size={14} />}
-            key={`suggestion_thing_${thing.title}`}
-          >
-            {thing.title}
-          </Menu.Item>
-        ))}
-      {filteredSuggestedPeople.length > 0 && (
-        <Group>
-          {filteredSuggestedPeople.map(person => (
-            <PersonAvatar person={person} key={`suggestion_person_${person.text}`} />
+
+        {filteredSuggestedUserAlbums.length > 0 &&
+          filteredSuggestedUserAlbums.slice(0, 2).map(album => (
+            <Group className={classes.item} key={`suggestion_album_${album.id}`}>
+              <Album size={14} />
+              <Link to={`/useralbum/${album.id}`} key={`suggestion_place_${album.title}`}>
+                {album.title}
+              </Link>
+            </Group>
           ))}
-        </Group>
-      )}
-      {albumsThingList.length === 0 && searchText.length > 0 && (
-        <Menu.Item icon={<Search size={14} />}>
-          <Group>
+
+        {filteredSuggestedPlaces.length > 0 &&
+          filteredSuggestedPlaces.slice(0, 2).map(place => (
+            <Group className={classes.item} key={`suggestion_place_${place.id}`}>
+              <Map size={14} />
+              <Link to={`/place/${place.id}`} key={`suggestion_place_${place.title}`}>
+                {place.title}
+              </Link>
+            </Group>
+          ))}
+
+        {filteredSuggestedThings.length > 0 &&
+          filteredSuggestedThings.slice(0, 2).map(thing => (
+            <Group className={classes.item} key={`suggestion_thing_${thing.id}`}>
+              <Tag size={14} />
+              <Link to={`/thing/${thing.id}`} key={`suggestion_thing_${thing.title}`}>
+                {thing.title}
+              </Link>
+            </Group>
+          ))}
+
+        {filteredSuggestedPeople.length > 0 && (
+          <Group className={classes.faces}>
+            {filteredSuggestedPeople.map(person => (
+              <PersonAvatar person={person} key={`suggestion_person_${person.text}`} />
+            ))}
+          </Group>
+        )}
+
+        {albumsThingList.length === 0 && searchText.length > 0 && (
+          <Group className={classes.item}>
             <Loader size={14} />
             {t("search.loading")}
           </Group>
-        </Menu.Item>
-      )}
-    </Menu>
+        )}
+      </Popover.Dropdown>
+    </Popover>
   );
 }
