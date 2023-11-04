68d64c6c7e0cdde700cd01366af36c2a32b935c3
sickelap <sickelap@pm.me>
2023-11-04 13:49:38 +0000

    enable no-restricted-syntax rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,6 @@ module.exports = {
   plugins: ["prettier"],
   rules: {
     "no-await-in-loop": "warn",
-    "no-restricted-syntax": "warn",
     "no-param-reassign": "warn",
     "no-underscore-dangle": "warn",
     "import/prefer-default-export": "off",
