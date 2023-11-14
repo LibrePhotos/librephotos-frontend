6fd5e65779c7cd73cb649afa984bcf75faaac2e5
sickelap <sickelap@pm.me>
2023-11-14 10:13:00 +0000

    fix explorer

--- src/layouts/albums/Explorer.tsx
+++ src/layouts/albums/Explorer.tsx
@@ -1,93 +1,118 @@
 import { Box, Button, Group, Stack, Title } from "@mantine/core";
-import React, { useEffect } from "react";
+import React from "react";
 import { useTranslation } from "react-i18next";
-import { push } from "redux-first-history";
+import { Link, useNavigate } from "react-router-dom";
 import { Ballon } from "tabler-icons-react";
 
-import { fetchAutoAlbumsList, fetchUserAlbumsList } from "../../actions/albumsActions";
+import { useFetchPeopleAlbumsQuery } from "../../api_client/albums/people";
+import { useFetchPlacesAlbumsQuery } from "../../api_client/albums/places";
+import { useFetchThingsAlbumsQuery } from "../../api_client/albums/things";
 import { Tile } from "../../components/Tile";
-import { useAppDispatch, useAppSelector } from "../../store/store";
 import { HeaderComponent } from "./HeaderComponent";
 
 export function Explorer() {
-  const { albumsAutoList, fetchingAlbumsAutoList } = useAppSelector(store => store.albums);
-  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
   const { t } = useTranslation();
   const entrySquareSize = 200;
-
-  useEffect(() => {
-    dispatch(fetchAutoAlbumsList());
-    dispatch(fetchUserAlbumsList());
-  }, []);
-
-  function getAutoAlbums(albums) {
-    return albums.slice(0, 19).map(album => (
-      <Tile
-        onClick={() => {
-          dispatch(push(`/album/${album.id}`));
-        }}
-        key={album.id}
-        video={album.cover_photos[0].video === true}
-        height={entrySquareSize - 10}
-        width={entrySquareSize - 10}
-        image_hash={album.cover_photos[0].image_hash}
-      />
-    ));
-  }
+  const { data: peopleAlbums, isFetching: isFetchingPeopleAlbums } = useFetchPeopleAlbumsQuery();
+  const { data: thingsAlbums, isFetching: isFetchingThingsAlbums } = useFetchThingsAlbumsQuery();
+  const { data: placesAlbums, isFetching: isFetchingPlacesAlbums } = useFetchPlacesAlbumsQuery();
 
   return (
     <Stack>
       <HeaderComponent
         icon={<Ballon size={50} />}
-        fetching={fetchingAlbumsAutoList}
+        fetching={isFetchingPeopleAlbums || isFetchingThingsAlbums || isFetchingPlacesAlbums}
         title="Explorer"
         subtitle="Explore your photos."
       />
-      <Title order={3}>{t("people")}</Title>
-      <Group>
-        {getAutoAlbums(albumsAutoList)}
-        <Box color="gray">
-          <Button
-            variant="light"
-            color="gray"
-            onClick={() => {
-              dispatch(push(`/album/`));
-            }}
-          >
-            Show More
-          </Button>
-        </Box>
-      </Group>
-      <Title order={3}>{t("things")}</Title>
-      <Group>
-        {getAutoAlbums(albumsAutoList)}
-        <Box color="gray">
-          <Button
-            variant="light"
-            color="gray"
-            onClick={() => {
-              dispatch(push(`/album/`));
-            }}
-          >
-            Show More
-          </Button>
-        </Box>
-      </Group>
-      <Title order={3}>{t("places")}</Title>
-      <Group>
-        {getAutoAlbums(albumsAutoList)}
-        <Box color="gray">
-          <Button
-            variant="light"
-            color="gray"
-            onClick={() => {
-              dispatch(push(`/album/`));
-            }}
-          >
-            Show More
-          </Button>
-        </Box>
-      </Group>
+
+      {!isFetchingPeopleAlbums && (
+        <>
+          <Title order={3}>{t("people")}</Title>
+          <Group>
+            {peopleAlbums?.slice(0, 19).map(album => (
+              <Link key={album.key} to={`/person/${album.key}/`}>
+                <Tile
+                  video={album.video === true}
+                  height={entrySquareSize - 10}
+                  width={entrySquareSize - 10}
+                  image_hash={album.face_photo_url}
+                />
+              </Link>
+            ))}
+            <Box color="gray">
+              <Button
+                variant="light"
+                color="gray"
+                onClick={() => {
+                  navigate(`/people/`);
+                }}
+              >
+                Show More
+              </Button>
+            </Box>
+          </Group>
+        </>
+      )}
+
+      {!isFetchingThingsAlbums && (
+        <>
+          <Title order={3}>{t("things")}</Title>
+          <Group>
+            {thingsAlbums?.slice(0, 19).map(album => (
+              <Link key={album.id} to={`/thing/${album.id}/`}>
+                <Tile
+                  video={album.cover_photos[0].video === true}
+                  height={entrySquareSize - 10}
+                  width={entrySquareSize - 10}
+                  image_hash={album.cover_photos[0].image_hash}
+                />
+              </Link>
+            ))}
+            <Box color="gray">
+              <Button
+                variant="light"
+                color="gray"
+                onClick={() => {
+                  navigate(`/things/`);
+                }}
+              >
+                Show More
+              </Button>
+            </Box>
+          </Group>
+        </>
+      )}
+
+      {!isFetchingPlacesAlbums && (
+        <>
+          <Title order={3}>{t("places")}</Title>
+          <Group>
+            {placesAlbums?.slice(0, 19).map(album => (
+              <Link key={album.id} to={`/place/${album.id}/`}>
+                <Tile
+                  video={album.cover_photos[0].video === true}
+                  height={entrySquareSize - 10}
+                  width={entrySquareSize - 10}
+                  image_hash={album.cover_photos[0].image_hash}
+                />
+              </Link>
+            ))}
+            <Box color="gray">
+              <Button
+                variant="light"
+                color="gray"
+                onClick={() => {
+                  navigate(`/places/`);
+                }}
+              >
+                Show More
+              </Button>
+            </Box>
+          </Group>
+        </>
+      )}
     </Stack>
   );
 }
