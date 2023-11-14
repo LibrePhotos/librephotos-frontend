2832da5fac283c5bf66303cdecf675e63924374b
sickelap <sickelap@pm.me>
2023-11-14 09:16:50 +0000

    place albums RTK

--- src/actions/albumsActions.ts
+++ src/actions/albumsActions.ts
@@ -1,24 +1,20 @@
 import { showNotification } from "@mantine/notifications";
 import _ from "lodash";
 import type { Dispatch } from "react";
-import { z } from "zod";
 
 import { Server } from "../api_client/apiClient";
 import i18n from "../i18n";
 import { PhotosetType } from "../reducers/photosReducer";
 import type { AppDispatch } from "../store/store";
 import { addTempElementsToGroups, getPhotosFlatFromGroupedByDate } from "../util/util";
-import type { AutoAlbumInfo, UserAlbumDetails, UserAlbumInfo } from "./albumActions.types";
+import type { AutoAlbumInfo, UserAlbumInfo } from "./albumActions.types";
 import {
   FetchAutoAlbumsListResponseSchema,
   FetchDateAlbumsListResponseSchema,
   FetchUserAlbumsListResponseSchema,
   FetchUserAlbumsSharedResponseSchema,
-  PlaceAlbumSchema,
-  UserAlbumInfoSchema,
-  UserAlbumSchema,
 } from "./albumActions.types";
-import type { DatePhotosGroup, IncompleteDatePhotosGroup } from "./photosActions.types";
+import type { IncompleteDatePhotosGroup } from "./photosActions.types";
 import { IncompleteDatePhotosGroupSchema } from "./photosActions.types";
 
 export function fetchUserAlbumsList() {
@@ -39,51 +35,8 @@ export function fetchUserAlbumsList() {
   };
 }
 
-export const FETCH_USER_ALBUM = "FETCH_USER_ALBUM";
 export const FETCH_USER_ALBUM_FULFILLED = "FETCH_USER_ALBUM_FULFILLED";
 export const FETCH_USER_ALBUM_REJECTED = "FETCH_USER_ALBUM_REJECTED";
-
-export function fetchUserAlbum(album_id: number) {
-  return function cb(dispatch: Dispatch<any>) {
-    dispatch({ type: FETCH_USER_ALBUM });
-    Server.get(`albums/user/${album_id}/`)
-      .then(response => {
-        const data = UserAlbumSchema.parse(response.data);
-        const photosGroupedByDate: DatePhotosGroup[] = data.grouped_photos;
-        const albumDetails: UserAlbumDetails = data;
-        dispatch({
-          type: FETCH_USER_ALBUM_FULFILLED,
-          payload: {
-            photosGroupedByDate,
-            photosFlat: getPhotosFlatFromGroupedByDate(photosGroupedByDate),
-            albumDetails,
-          },
-        });
-      })
-      .catch(err => {
-        dispatch({ type: FETCH_USER_ALBUM_REJECTED, payload: err });
-      });
-  };
-}
-
-const PlaceAlbumResponseSchema = z.object({ results: PlaceAlbumSchema });
-
-export function fetchPlaceAlbum(album_id: string) {
-  return function cb(dispatch: Dispatch<any>) {
-    dispatch({ type: "FETCH_PLACE_ALBUMS" });
-    Server.get(`albums/place/${album_id}/`)
-      .then(response => {
-        const data = PlaceAlbumResponseSchema.parse(response.data);
-        dispatch({
-          type: "FETCH_PLACE_ALBUMS_FULFILLED",
-          payload: data,
-        });
-      })
-      .catch(err => {
-        dispatch({ type: "FETCH_PLACE_ALBUMS_REJECTED", payload: err });
-      });
-  };
-}
 
 export function fetchAutoAlbumsList() {
   return function cb(dispatch: Dispatch<any>) {
@@ -202,39 +155,6 @@ export function deleteAllAutoAlbum() {
       })
       .catch(err => {
         dispatch({ type: "DELETE_ALL_AUTO_ALBUM_REJECTED", payload: err });
-      });
-  };
-}
-
-// share user album
-export function setUserAlbumShared(album_id: number, target_user_id: string, val_shared: boolean) {
-  return function cb(dispatch: Dispatch<any>) {
-    dispatch({ type: "SET_ALBUM_USER_SHARED" });
-    Server.post("useralbum/share/", { shared: val_shared, album_id, target_user_id })
-      .then(response => {
-        const userAlbumInfo: UserAlbumInfo = UserAlbumInfoSchema.parse(response.data);
-        dispatch({
-          type: "SET_ALBUM_USER_SHARED_FULFILLED",
-          payload: userAlbumInfo,
-        });
-        dispatch(fetchUserAlbum(album_id));
-
-        if (val_shared) {
-          showNotification({
-            message: i18n.t("toasts.sharingalbum"),
-            title: i18n.t("toasts.sharingalbumtitle"),
-            color: "teal",
-          });
-        } else {
-          showNotification({
-            message: i18n.t("toasts.unsharingalbum"),
-            title: i18n.t("toasts.unsharingalbumtitle"),
-            color: "teal",
-          });
-        }
-      })
-      .catch(err => {
-        dispatch({ type: "SET_ALBUM_USER_SHARED_FULFILLED", payload: err });
       });
   };
 }
