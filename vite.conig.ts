0040b27bf469359c13ac5d9c36b64a6e524ca2d1
sickelap <sickelap@pm.me>
2024-01-24 20:45:32 +0000

    vite wip

---
+++ vite.conig.ts
@@ -0,0 +1,12 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import viteTsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  base: "",
+  plugins: [react(), viteTsconfigPaths()],
+  server: {
+    open: true,
+    port: 3008,
+  },
+});
