75197f3538a0570e2aecbbbda69c8184366d97f2
sickelap <sickelap@pm.me>
2024-01-25 13:32:10 +0000

    add and enable vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 import viteTsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   base: "/",
@@ -8,5 +8,17 @@ export default defineConfig({
   server: {
     host: "0.0.0.0",
     port: 3000,
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.ts",
+    css: true,
+    reporters: ["verbose"],
+    coverage: {
+      reporter: ["text", "json", "html"],
+      include: ["src/**/*"],
+      exclude: [],
+    },
   },
 });
